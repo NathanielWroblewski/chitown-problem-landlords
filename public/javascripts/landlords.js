0494f22014269ab38d7f8bf25f5f1fef6aa90c47
Nathaniel Wroblewski <nwroblewski@kissmetrics.com>
2015-02-02 22:03:56 -0600

    open Github Pages page

--- public/javascripts/landlords.js
+++ public/javascripts/landlords.js
@@ -2,7 +2,9 @@
   google.maps.event.addDomListener(window, 'load', initialize)
 
   function initialize() {
-    get('/public/datasets/problem_landlords.json', function(response) {
+    var DATA = '/chitown-problem-landlords/public/datasets/problem_landlords.json'
+
+    get(DATA, function(response) {
       var chitown = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 41.8369, lng: -87.6847 },
         zoom: 11
@@ -32,109 +34,61 @@
 
       chitown.setOptions({
         styles: [
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "hue": "#F1FF00"
-            },
-            {
-                "saturation": -27.4
-            },
-            {
-                "lightness": 9.4
-            },
-            {
-                "gamma": 1
-            }
+          {
+            featureType: 'landscape',
+            stylers: [
+              { hue: '#F1FF00'},
+              { saturation: -27.4 },
+              { lightness: 9.4 },
+              { gamma: 1 }
+            ]
+          },
+          {
+            featureType: 'road.highway',
+            stylers: [
+              { hue: '#0099FF' },
+              { saturation: -20 },
+              { lightness: 36.4 },
+              { gamma: 1 }
+            ]
+          },
+          {
+            featureType: 'road.arterial',
+               stylers: [
+                 { hue: '#00FF4F' },
+                 { saturation: 0 },
+                 { lightness: 0 },
+                 { gamma: 1 }
+               ]
+           },
+          {
+            featureType: 'road.local',
+               stylers: [
+                 { hue: '#FFB300' },
+                 { saturation: -38 },
+                 { lightness: 11.2 },
+                 { gamma: 1 }
+               ]
+           },
+          {
+            featureType: 'water',
+               stylers: [
+                 { hue: '#00B6FF' },
+                 { saturation: 4.2 },
+                 { lightness: -63.4 },
+                 { gamma: 1 }
+               ]
+           },
+          {
+            featureType: 'poi',
+               stylers: [
+                 { hue: '#9FFF00' },
+                 { saturation: 0 },
+                 { lightness: 0 },
+                 { gamma: 1 }
+               ]
+          }
         ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "hue": "#0099FF"
-            },
-            {
-                "saturation": -20
-            },
-            {
-                "lightness": 36.4
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "stylers": [
-            {
-                "hue": "#00FF4F"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 0
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "hue": "#FFB300"
-            },
-            {
-                "saturation": -38
-            },
-            {
-                "lightness": 11.2
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "hue": "#00B6FF"
-            },
-            {
-                "saturation": 4.2
-            },
-            {
-                "lightness": -63.4
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "hue": "#9FFF00"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 0
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    }
-]
       })
     })
   }
